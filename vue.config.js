7a86a2ad968810bee8c2e0305357853167e38b36
William <lisirui086@gmail.com>
2023-06-30 12:34:42 +0800

    统一页面标题

--- vue.config.js
+++ vue.config.js
@@ -7,12 +7,17 @@ module.exports = defineConfig({
   // 开启代理
   devServer: {
     host: 'localhost',
-    port:'8080',
+    port: '8080',
     proxy: {
       '/api': {
-
-        target: 'http://gmall-h5-api.atguigu.cn'
-      }
-    }
-  }
+        target: 'http://gmall-h5-api.atguigu.cn',
+      },
+    },
+  },
+  chainWebpack: (config) => {
+    config.plugin('html').tap((args) => {
+      args[0].title = '任真美好线上商城'
+      return args
+    })
+  },
 })
